134bdac2c6a46c007f84c31b8c51213c131e049d
Max Coldrick <maxcoldrick@gmail.com>
2019-06-25 20:07:47 +0100

    First commit + db setup

---
+++ db/migrate/20190625190413_create_foods.rb
@@ -0,0 +1,10 @@
+class CreateFoods < ActiveRecord::Migration[5.2]
+  def change
+    create_table :foods do |t|
+      t.text :dish
+      t.text :measurement
+
+      t.timestamps
+    end
+  end
+end
