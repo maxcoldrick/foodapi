92511f0d604d93b98afa648ea7a181a4025641ba
Max Coldrick <maxcoldrick@gmail.com>
2019-07-23 21:40:32 +0100

    added repeat connection strategy to java app

---
+++ databaseFillerUpper/src/main/java/RabbitMQ/Connector.java
@@ -0,0 +1,42 @@
+package RabbitMQ;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Connector {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void waitForMessages() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("rabbitmq");
+
+        boolean repeat=true;
+        while(repeat)
+        {
+            try {
+                Connection connection = factory.newConnection();
+                Channel channel = connection.createChannel();
+
+                channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+                System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+                DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                    String message = new String(delivery.getBody(), "UTF-8");
+                    System.out.println(" [x] Received '" + message + "'");
+                };
+                channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+
+                repeat=false;
+            }
+            catch (IOException e) {
+                System.out.println("Failed to connect. Retrying in 10secs...");
+                Thread.sleep(10000);
+            }
+        }
+    }
+}
